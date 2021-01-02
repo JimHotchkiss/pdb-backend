18cfbbf17b7d83e780b7ce9ad50781c73f2558a1
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2021-01-02 09:33:04 -0500

    Update to make new branch for cookies.

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,13 +3,19 @@ class Api::V1::SessionsController < ApplicationController
     def create 
         @user = User.find_by(username: login_params[:username])
         if @user && @user.authenticate(login_params[:password])
-            session[:user_Id] = @user.Invalid
-            render json: @user 
+            session[:user_id] = @user.id
+            render json: {user: @user, message: session[:user_id]} 
         else 
             render json: {
                 error: "Invalid login information"
             }
         end 
+    end 
+
+    def current_user
+        binding.pry
+        @user = User.find_by(id: session[:user_id])
+        render json: @user
     end 
 
         private 
